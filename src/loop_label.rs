d18e80e21a8d9e813710504594935e82c087fcca
xqb64 <x@xqb.ch>
2024-09-08 17:02:31 +0200

    wip: checkpoint

--- src/loop_label.rs
+++ src/loop_label.rs
@@ -8,21 +8,21 @@ pub trait Label {
 
 impl Label for ProgramStatement {
     fn label(&self, current_label: String) -> Result<BlockItem> {
-        let labeled_items = self.stmts.iter().map(|item| label_block_item(item, current_label.clone())).collect::<Result<Vec<_>>>()?;
+        let labeled_items = self.stmts.iter().map(|item| item.label(current_label.clone())).collect::<Result<Vec<_>>>()?;
         Ok(BlockItem::Statement(Statement::Program(ProgramStatement { stmts: labeled_items })))
     }
 }
 
 impl Label for BlockStatement {
     fn label(&self, current_label: String) -> Result<BlockItem> {
-        let labeled_items = self.stmts.iter().map(|item| label_block_item(item, current_label.clone())).collect::<Result<Vec<_>>>()?;
+        let labeled_items = self.stmts.iter().map(|item| item.label(current_label.clone())).collect::<Result<Vec<_>>>()?;
         Ok(BlockItem::Statement(Statement::Compound(BlockStatement { stmts: labeled_items })))
     }
 }
 
 impl Label for IfStatement {
     fn label(&self, current_label: String) -> Result<BlockItem> {
-        let labeled_then_body = label_block_item(&self.then_branch, current_label.clone())?;
+        let labeled_then_body = self.then_branch.label(current_label.clone())?;
         let labeled_else_body = label_optional_block_item(&self.else_branch, current_label.clone())?;
         Ok(BlockItem::Statement(Statement::If(IfStatement { condition: self.condition.clone(), then_branch: labeled_then_body.into(), else_branch: labeled_else_body.into() })))
     }
@@ -51,7 +51,7 @@ impl Label for ContinueStatement {
 impl Label for WhileStatement {
     fn label(&self, _current_label: String) -> Result<BlockItem> {
         let new_label = format!("While.{}", make_temporary());
-        let labeled_body = label_block_item(&self.body, new_label.clone())?;
+        let labeled_body = self.body.label(new_label.clone())?;
 
         Ok(BlockItem::Statement(Statement::While(WhileStatement { label: new_label, condition: self.condition.clone(), body: labeled_body.into() })))
     }
@@ -60,7 +60,7 @@ impl Label for WhileStatement {
 impl Label for DoWhileStatement {
     fn label(&self, _current_label: String) -> Result<BlockItem> {
         let new_label = format!("DoWhile.{}", make_temporary());
-        let labeled_body = label_block_item(&self.body, new_label.clone())?;
+        let labeled_body = self.body.label(new_label.clone())?;
 
         Ok(BlockItem::Statement(Statement::DoWhile(DoWhileStatement { label: new_label, body: labeled_body.into(), condition: self.condition.clone() })))
     }
@@ -69,7 +69,7 @@ impl Label for DoWhileStatement {
 impl Label for ForStatement {
     fn label(&self, _current_label: String) -> Result<BlockItem> {
         let new_label = format!("For.{}", make_temporary());
-        let labeled_body = label_block_item(&self.body, new_label.clone())?;
+        let labeled_body = self.body.label(new_label.clone())?;
 
         Ok(BlockItem::Statement(Statement::For(ForStatement { label: new_label, init: self.init.clone(), condition: self.condition.clone(), post: self.post.clone(), body: labeled_body.into() })))
     }
@@ -105,29 +105,33 @@ impl Label for Statement {
     }
 }
 
-fn label_block_item(block_item: &BlockItem, current_label: String) -> Result<BlockItem> {
-    match block_item {
-        BlockItem::Statement(s) => {
-            let labeled_statement = s.label(current_label)?;
-            Ok(labeled_statement)
-        }
-
-        BlockItem::Declaration(decl) => {
-            match decl {
-                Declaration::Function(FunctionDeclaration { name, body }) => {
-                    let labeled_body = label_optional_block_item(&body, current_label)?;
-                    Ok(BlockItem::Declaration(Declaration::Function(FunctionDeclaration { name: name.clone(), body: labeled_body.into() })))
-                }
-                Declaration::Variable(var_decl) => Ok(BlockItem::Declaration(Declaration::Variable(var_decl.clone()))),
+impl Label for BlockItem {
+    fn label(&self, current_label: String) -> Result<BlockItem> {
+        match self {
+            BlockItem::Statement(s) => {
+                let labeled_statement = s.label(current_label)?;
+                Ok(labeled_statement)
             }
-        },
+    
+            BlockItem::Declaration(decl) => {
+                match decl {
+                    Declaration::Function(FunctionDeclaration { name, body }) => {
+                        let labeled_body = label_optional_block_item(&body, current_label)?;
+                        Ok(BlockItem::Declaration(Declaration::Function(FunctionDeclaration { name: name.clone(), body: labeled_body.into() })))
+                    }
+                    Declaration::Variable(var_decl) => Ok(BlockItem::Declaration(Declaration::Variable(var_decl.clone()))),
+                }
+            },
+        }
+    
     }
 }
+
 
 fn label_optional_block_item(block_item: &Option<BlockItem>, current_label: String) -> Result<Option<BlockItem>> {
     match block_item {
         Some(b) => {
-            let labeled_block_item = label_block_item(b, current_label)?;
+            let labeled_block_item = b.label( current_label)?;
 
             Ok(Some(labeled_block_item))
         }
