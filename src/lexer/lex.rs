a175dc320f865d42644f6788786c623ba9ee6908
xqb64 <x@xqb.ch>
2025-06-03 11:26:16 +0200

    chore: rm commented code

--- src/lexer/lex.rs
+++ src/lexer/lex.rs
@@ -671,16 +671,3 @@ impl ToString for Const {
     }
 }
 
-// impl Const {
-//     pub fn to_u32(&self) -> Option<u32> {
-//         match self {
-//             Const::Int(x) if *x >= 0 => Some(*x as u32),
-//             Const::UInt(x) => Some(*x as u32),
-//             Const::Long(x) if *x >= 0 => Some(*x as u32),
-//             Const::ULong(x) => Some(*x as u32),
-//             Const::Char(x) if *x >= 0 => Some(*x as u32),
-//             Const::UChar(x) => Some(*x as u32),
-//             _ => None, // Includes Double and invalid conversions
-//         }
-//     }
-// }
